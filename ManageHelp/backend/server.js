54869f9f00609414d12ddda254be5671c6412817
mwhiatt <89279653+mwhiatt@users.noreply.github.com>
2023-02-15 12:40:01 -0500

    Add files via upload

---
+++ ManageHelp/backend/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workspaceRoutes = require('./routes/workspaces')
+
+// express app stored in app constant
+const app = express()
+
+// middleware (executes between request receipt and response)
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+
+//routes
+app.use('/api/workspaces', workspaceRoutes)
+
+// connect to database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db and listening on port 4000')
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
