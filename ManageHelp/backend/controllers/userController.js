a11a296c976e1b195bd7932d8518c18e5bf82d52
mwhiatt <mwhiatt@purdue.edu>
2023-02-21 00:53:01 -0500

    added login functionality

--- ManageHelp/backend/controllers/userController.js
+++ ManageHelp/backend/controllers/userController.js
@@ -1,8 +1,21 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (id) => {
+    return jwt.sign({_id: id}, process.env.SECRET, { expiresIn: '1d' })
+}
 
 // login user
 const loginUser = async (req, res) => {
-    res.json({mssg: 'login user'})
+    const { email, password } = req.body
+
+    try {
+        const user = await User.login(email, password)
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 //signup user
@@ -11,7 +24,8 @@ const signupUser = async (req, res) => {
 
     try {
         const user = await User.signup(email, password)
-        res.status(200).json({email, user})
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
--- ManageHelp/backend/models/userModel.js
+++ ManageHelp/backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -17,6 +18,17 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function (email, password) {
+    // validation
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password too weak')
+    }
+    
     //check if email exists
     const exists = await this.findOne({ email })
 
@@ -29,6 +41,26 @@ userSchema.statics.signup = async function (email, password) {
     const hash = await bcrypt.hash(password, salt)
 
     const user = await this.create({ email, password: hash })
+
+    return user
+}
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+
+    const user = await this.findOne({ email })
+
+    if (!user) {
+        throw Error('Email not attached to account, try signing up')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+        throw Error('Incorrect credientials')
+    }
 
     return user
 }
