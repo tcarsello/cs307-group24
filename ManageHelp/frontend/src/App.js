ed44c29fd204bafff16ab600aabf7e7399ad2d91
ssharan31 <34865027+ssharan31@users.noreply.github.com>
2023-02-20 19:22:31 -0500

    Update App.js
    Added the settings route

--- ManageHelp/frontend/src/App.js
+++ ManageHelp/frontend/src/App.js
@@ -3,21 +3,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 //pages and components
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
-
+import Settings from './pages/Settings';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
-          <Routes>
+          <Routes> 
             <Route 
               path="/"
               element={<Home />}
             />
-            <Route
+            <Route 
               path="/"
-              element={<serInfo />}
+              element={< Settings/>}
             />
           </Routes>
         </div> 
