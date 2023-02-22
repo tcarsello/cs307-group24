72f36f2fe10752ba29c85dba4b6a57e6002595c6
ssharan31 <34865027+ssharan31@users.noreply.github.com>
2023-02-21 21:30:41 -0500

    Update Settings.js
    Added button click functionality so it stores user input for updated username and passwords in their respective state variables

--- ManageHelp/frontend/src/pages/Settings.js
+++ ManageHelp/frontend/src/pages/Settings.js
@@ -1,13 +1,31 @@
 import { useEffect } from "react"
 import { useWorkspaceContext } from "../hooks/useWorkspaceContext"
+import { useRef } from "react"
+import { useState } from "react"
 
 // components
-import WorkspaceDetails from "../components/WorkspaceDetails"
-import CreateWorkspaceForm from "../components/CreateWorkspaceForm"
+
 import UserInformationForm from "../components/UserInfomationForm"
 
 const Settings = () => {
   const { workspaces, dispatch } = useWorkspaceContext()
+  //Defining state variables to hold the updated usernames and passwords
+  const [inputUsernameValue, setInputUsernameValue] = useState(""); 
+  const [inputPasswordValue, setInputPasswordValue] = useState("");
+
+  //Defining functions to handle changes in the input and changes in the button
+  const handleUsernameChange = (event) => {
+    setInputUsernameValue(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setInputPasswordValue(event.target.value);
+  };
+  const handleUsernameAlert = () => {
+    alert(inputUsernameValue);
+  };
+  const handlePasswordAlert = () => {
+    alert(inputPasswordValue);
+  };
 
   useEffect(() => {
     const fetchWorkspaces = async () => {
@@ -23,24 +41,27 @@ const Settings = () => {
   }, [dispatch])
 
   return (
+
     <div className="home">
       <div className="workspaces">
       <h1>Settings</h1>
-        <h2>Change Username and Password</h2>
+      <h2>Change Username and Password</h2>
             <h3>Enter new username</h3>
                 <form>
                 <fieldset>
-                    <input name="Name"/>
+                    <input value={inputUsernameValue} type="text" onChange={handleUsernameChange}/>
                 </fieldset>
-                <button type="submit">Submit</button>
+                <button onClick={handleUsernameAlert}>Update</button>
                 </form>
-            <h3>Enter new password</h3>
+            <h3>Enter new Password</h3>
                 <form>
                 <fieldset>
-                    <input name="Name"/>
+                    <input value={inputPasswordValue} type="text" onChange={handlePasswordChange}/>
                 </fieldset>
-                <button type="submit">Submit</button>
+                <button onClick={handlePasswordAlert}>Update</button>
                 </form>
+                
+            
       </div>
       <UserInformationForm />
     </div>
