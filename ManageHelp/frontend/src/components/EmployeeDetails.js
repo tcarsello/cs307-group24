ed6309653e790b3be40d7cbcfd165696d9dd5382
mwhiatt <mwhiatt@purdue.edu>
2023-03-02 14:53:24 -0500

    Added job title and pay rate to employee cards

--- ManageHelp/frontend/src/components/EmployeeDetails.js
+++ ManageHelp/frontend/src/components/EmployeeDetails.js
@@ -1,4 +1,20 @@
+import { useEffect, useState } from 'react'
+
+const getEmployeeData = async (user_id, workspace_id) => {
+
+    //console.log(`/api/employeedata/${workspace_id}/${user_id}`)
+    const response = await fetch(`/api/employeedata/${workspace_id}/${user_id}`, {
+        method: 'GET',
+    })
+
+    const json = await response.json()
+    return json
+
+}
+
 const EmployeeDetails = ({ workspace, employee }) => {
+    const [employeeData, setEmployeeData] = useState('')
+    const [runUseEffect, setRunUseEffect] = useState('')
 
     let role = ''
     if (workspace.employee_list.includes(employee._id)) {
@@ -6,13 +22,21 @@ const EmployeeDetails = ({ workspace, employee }) => {
     } else if (workspace.manager_list.includes(employee._id)) {
         role = 'Manager'
     } else {
-        role = 'Undefined'
+        role = 'Admin'
     }
+
+    useEffect(() => {
+        getEmployeeData(workspace._id, employee._id). then(ed => {
+            setEmployeeData(ed)
+        })
+    }, [runUseEffect])
 
     return (
         <div className="workspace-details">
             <p><strong>Email: </strong>{employee.email}</p>
             <p><strong>Role: </strong>{role}</p>
+            <p><strong>Job Title: </strong>{employeeData.job_title}</p>
+            <p><strong>Pay Rate: </strong>{employeeData.pay_rate}</p>
         </div>
     )
 }
