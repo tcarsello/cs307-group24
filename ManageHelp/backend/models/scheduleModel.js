64f138e0a752934e0fd2a9eea5c2d257ff907e60
Thomas Carsello <tkcarsello@gmail.com>
2023-03-23 12:47:29 -0400

    Wrote backend requests for shift schema

--- ManageHelp/backend/models/scheduleModel.js
+++ ManageHelp/backend/models/scheduleModel.js
@@ -19,6 +19,24 @@ const ScheduleSchema = new Schema({
         required: true,
     },
 
+    published: {
+        type: Boolean,
+        required: true
+    }
+
 }, {timestamps: true})
+
+ScheduleSchema.statics.createSchedule = async function (workspace_id, week_date, published) {
+
+    if (!workspace_id) throw new Error('Must specify workspace_id')
+    if (!week_date) throw new Error('Must specify week_date')
+
+    const date_conv = new Date(week_date)
+
+    const schedule = await this.create({workspace_id: workspace_id, week_date: date_conv, shift_list: [], published: published})
+
+    return schedule
+
+}
 
 module.exports = mongoose.model('Schedule', ScheduleSchema)
