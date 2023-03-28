0847d61f32a7198aa9cf86920699dbe6686b0bd8
Thomas Carsello <tkcarsello@gmail.com>
2023-03-27 23:46:22 -0400

    Backend request for getting all day off requests by workspace id

--- ManageHelp/backend/routes/dayOffRequest.js
+++ ManageHelp/backend/routes/dayOffRequest.js
@@ -4,11 +4,14 @@ const router = express.Router()
 const {
     createDayOffRequest,
     approveDayOffRequest,
-    rejectDayOffRequest
+    rejectDayOffRequest,
+    getAllByWorkspace,
 } = require('../controllers/dayOffRequestController')
 
 router.post('/', createDayOffRequest)
 router.post('/approve/:id', approveDayOffRequest)
 router.post('/reject/:id', rejectDayOffRequest)
+
+router.get('/:id', getAllByWorkspace)
 
 module.exports = router
